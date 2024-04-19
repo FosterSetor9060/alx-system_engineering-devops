6e8e5423c949b12cc6cc59b3b5ea9d346c8d1b63
Foster Setor <fostersetor6@gmail.com>
2024-04-19 06:00:50 -0700

    permission accessed

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
-# Creating A File
+# Creating A File /tmp
 file { '/tmp/school':
-	ensure	=> 'file',
-      	mode	=> '0744',
-	owner	=> 'www-data',
-	group	=> 'www-data',
-	content => 'I love Puppet'
+    ensure  => 'file',
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0744',
+    content => 'I love Puppet',
 }

fda66f8f31c27d4db6e85fbafc06952eb6d1eb37
Foster Setor <fostersetor6@gmail.com>
2024-04-21 06:59:25 -0700

    os-configuration management system_components

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,9 @@
-#!/usr/bin/pup
-# Install unique version of flask (2.1.0)
-package {'flask':
+# This is a Puppet manifest file (1-install_a_package.pp) to install Flask using Puppet.
+# It ensures that Flask version 2.1.0 is installed.
+
+package { 'Flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
 }
+
 

1ad5f9a288c19777063407df825c22c76b5040c5
Foster Setor <fostersetor6@gmail.com>
2024-04-21 07:17:56 -0700

    os_cm settings file

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,9 +1,7 @@
-# This is a Puppet manifest file (1-install_a_package.pp) to install Flask using Puppet.
+#!/usr/bin/pup
 # It ensures that Flask version 2.1.0 is installed.
 
 package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
-
-

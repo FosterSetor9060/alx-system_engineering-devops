53a71ff6eccf2991db451631bf8b013a8431080a
Foster Setor <fostersetor6@gmail.com>
2024-04-22 15:51:06 -0700

    Resolved conflicts in 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
 #!/usr/bin/pup
-# It ensures that Flask version 2.1.0 is installed.
+# Ensuring that Flask version 2.1.0 is installed.
 package {'flask':
   ensure   => '2.1.0',
   provider => 'pip3'
